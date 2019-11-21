a134635fdc4b2cd5040bf88dcf24bcf2e6782374
BIT Student <classroom@bgit.rs>
2019-11-21 09:14:03 +0100

    first commit

---
+++ es6.js
@@ -0,0 +1,11 @@
+/*Write a function that capitalizes the first letter of each string argument it receives.
+	Function arguments: 'hello', 'there', 'ES', 6
+	Output: 'Hello', 'There', 'ES'
+*/
+
+
+git init
+git add README.md
+git commit - m "first commit"
+git remote add origin https://github.com/anaaleksic/web-app.git
+git push - u origin master
